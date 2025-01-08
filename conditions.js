f5331d231832e942e9b7f2c27ee4f4efaa038530
username <example@gmail.com>
2025-01-08 12:52:32 +0530

    added file

---
+++ conditions.js
@@ -0,0 +1,46 @@
+// for(let i=1;i<=5;i++){
+//     console.log("Helloworld");
+// }
+// let n=100
+// for(int i=1;i<=5;i++){
+//     console.log("Hello world");
+// if(i%2 == 0){
+//     console.log=(i +"is a even number");
+// }
+// else{
+//     console.log(i+"is a odd number")
+// }
+// }
+
+// for(let i=5;i>0;i--){
+//     console.log(5*i);
+// }
+
+// let i=1;
+// while(i<=5){
+//     console.log("Good Morning");
+//     i++;
+// }
+// let i=1;
+// do{
+//     console.log("please wake up and interact!!!!!");
+//     i++;
+// }while(i<=5);
+
+// function demo(){
+//     console.log("Hello i am function demo()")
+
+// }
+
+// function add1(a,b){
+//     // console.log(a);
+//     // console.log(b);
+//     console.log(a+b);
+// }
+// add1(5,6);
+// add1(7,8);
+
+function AddToCart(){
+    document.
+
+}
