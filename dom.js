47d2b75e575d3bb38571eff64aa2df20a5621f1a
username <example@gmail.com>
2025-01-24 11:39:36 +0530

    24/01/25

---
+++ dom.js
@@ -0,0 +1,9 @@
+// let ele = document.querySelectorAll(".test");
+// [...ele].map(element=>{
+//     console.log(element.innerText);
+// })
+
+let btn=document.getElementById("btn");
+btn.addEventListener("dblclick",()=>{
+    console.log("clicked two times");
+}) 
